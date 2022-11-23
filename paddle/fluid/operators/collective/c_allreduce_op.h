2841b7417e4bc028b1d2c1c31f92cca0bede23d2
JZ-LIANG <jianzhongliang10@gmail.com>
2022-11-23 18:57:51 +0800

    allreduce op ctx

--- paddle/fluid/operators/collective/c_allreduce_op.h
+++ paddle/fluid/operators/collective/c_allreduce_op.h
@@ -482,8 +482,9 @@ class CAllReduceOpCUDAKernel : public framework::OpKernel<T> {
 
     gpuStream_t stream = nullptr;
     if (ctx.Attr<bool>("use_calc_stream")) {
-      auto dev_ctx = platform::DeviceContextPool::Instance().Get(place);
-      stream = static_cast<phi::GPUContext*>(dev_ctx)->stream();
+      // auto dev_ctx = platform::DeviceContextPool::Instance().Get(place);
+      // stream = static_cast<phi::GPUContext*>(dev_ctx)->stream();
+      stream = static_cast<phi::GPUContext>(ctx.device_context()).stream();
     } else {
       stream = comm->stream();
     }
